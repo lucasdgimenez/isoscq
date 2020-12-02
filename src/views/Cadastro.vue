758d235f1325bc50403f1b4165153b33c85bd8e3
Dessenvolvimento IS <fernando.zanardi@integrasistemas.com.br>
2020-12-02 09:37:21 -0300

    #Commit tela de cadastro

---
+++ src/views/Cadastro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <form class="box">
+            <h1>Cadastro</h1>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Senha</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn button btn-lg btn-primary">Concluir</button><br/>
+            <span class="button">Concluir</span>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+
+</style>
